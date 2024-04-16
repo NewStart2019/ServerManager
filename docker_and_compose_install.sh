2195ab5b3a0ba1b0a6c2122cb768278a054719db
zhanqh <2855942332@qq.com>
2024-04-16 14:52:59 +0800

    优化docker和compose安装流程

--- docker_and_compose_install.sh
+++ docker_and_compose_install.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
-installDockerAndCompose() {
+installDocker(){
+  # 检查 Docker 是否已经安装
+  if ! command -v docker > /dev/null 2>&1
+  then
+      echo "Docker 未安装"
+      sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+      sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+      sudo yum makecache fast
+      sudo yum -y install docker-ce
+      sudo systemctl start docker
+      echo "安装docker成功"
+  else
+      echo "Docker 已安装"
+      sudo systemctl restart docker
+  fi
+  docker -v
+}
+
+installCompose() {
   start_time=$(date +%s) # 获取当前时间戳（秒）
   docker-compose -v
   if [ "$?" -ne 127 ]; then
     echo "已经安装过docker-compose"
     exit 0
   fi
-
-  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-  sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-  sudo yum makecache fast
-  sudo yum -y install docker-ce
-  sudo systemctl start docker
-  docker -v
-  echo "安装docker成功"
 
   yum install -y python3-pip #python3安装pip3!!!
   pip3 install -U pip setuptools
@@ -31,15 +41,15 @@ writeConfig() {
     echo '{
         "log-driver":"json-file",
         "log-opts": {"max-size":"500m", "max-file":"3"},
-        "insecure-registries": ["172.16.0.145:8083","172.16.0.145:8929"],
-        "registry-mirrors": ["http://172.16.0.145:8083/","http://172.16.0.145:8929/","https://registry.cn-hangzhou.aliyuncs.com/"]
+        "insecure-registries": ["172.16.0.197:8083","172.16.0.197:8929"],
+        "registry-mirrors": ["http://172.16.0.197:8083/","http://172.16.0.197:8929/","https://registry.cn-hangzhou.aliyuncs.com/"]
       }' | sudo tee "$file" >/dev/null
     echo "File $file created and content written."
   else
     echo "File $file already exists."
   fi
   sudo systemctl start docker
-  docker login -u admin -p s9AGdzFaSLXyQrD 172.16.0.145:8083
+  docker login -u admin -p s9AGdzFaSLXyQrD 172.16.0.197:8083
 }
 
 uninstallDockerAndCompose() {
@@ -58,6 +68,7 @@ uninstallDockerAndCompose() {
 }
 
 # 安装docker 和 compose
-installDockerAndCompose
+installDocker
+installCompose
 # 写docker配置文件 和 登录私有镜像仓库
 writeConfig
